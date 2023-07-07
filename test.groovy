e360bab8e7c0dc3349974eececde34400642a48a
Rayhan <mazharrayhan3737@gmail.com>
2023-07-07 08:46:43 +0600

    optimized way of twosum problem solution

--- test.groovy
+++ test.groovy
@@ -3,7 +3,7 @@ class Test {
         def arr = [2,4,6,7,83,9,4,22,42,67] as int[];
         String[] str = ["cat","dog","cow"];
         def res = 0;
-        def s = arr as Set;
+
         // for(int i=0;i<arr.size();i++){
         //     res+=a;rr[i];
         // }
--- two_sum.groovy
+++ two_sum.groovy
@@ -1,16 +1,23 @@
 class TwoSum{
     static void main(String[] args){
         int[] nums = [2,7,11,15];
-        int target = 9;
+        int target = 22;
 
         println twoSum(nums,target);
     }
 
     static int[] twoSum(int[] nums,int target) {
-         for(int i=0;i<nums.length-1;i++){
-            for(int j=i+1;j<nums.length;j++){
-                if(nums[i]+nums[j] == target) return new int[]{i,j};
-            }
-         }
+        //brute force solution O(n^2)
+        //  for(int i=0;i<nums.length-1;i++){
+        //     for(int j=i+1;j<nums.length;j++){
+        //         if(nums[i]+nums[j] == target) return new int[]{i,j};
+        //     }
+        //  }
+
+        def map = [:]
+        for(def i=0;i<nums.size();i++){
+            if(map.containsKey(nums[i])) return new int[]{map[nums[i]],i};
+            map[target-nums[i]]=i;
+        }
     }
 }
