08c06ba99c8e32cd6aec7e3c1990b32511c4bcef
Rayhan <mazharrayhan3737@gmail.com>
2023-07-07 09:57:40 +0600

    brute force fibonacci code and factorial code to practice recursive function

---
+++ factorial.groovy
@@ -0,0 +1,20 @@
+class Factorial {
+    static void main(String[] args){
+        // def arr = [2,4,6,7,83,9,4,22,42,67] as int[];
+
+        // for(int i=0;i<arr.size();i++){
+        //     res+=a;rr[i];
+        // }
+        // for(def num in arr) res+=num;
+        // for(def num : arr) res+=num;
+        // println res; 
+        // println arr.sum();
+
+        println factorial(10);
+    }
+
+    static int factorial(int n){
+        if(n<=1) return 1;
+        else return n * factorial(n-1);
+    }
+}
---
+++ fibonacci.groovy
@@ -0,0 +1,11 @@
+class Fibonacci {
+    static void main(String[] args){
+        println fib(20);
+    }
+
+    static int fib(int n){
+        if(n==0) return 0;
+        else if(n==1) return 1;
+        else return fib(n-1)+fib(n-2);
+    }
+}
